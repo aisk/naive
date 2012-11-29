fb57e5d3986bcf9014632f44e9e28e3892e6c16b
aisk <aisk1988@gmail.com>
2012-11-29 23:48:38 +0800

    add 'if' syntax

--- naive/compiler.py
+++ naive/compiler.py
@@ -18,9 +18,19 @@ class NameToken(Token):
     def names(self):
         return self.split('.')
 
-class SyntaxToken(Token):
+class ExprToken(Token):
     '''{%syntax%} '''
-    pass
+    @property
+    def tokens(self):
+        return self.split()
+    def __init__(self, s):
+        super(str, self).__init__(s)
+        if self.tokens[0] == 'if': self.type = 'if'
+        elif self.tokens[0] == 'else': self.type = 'else'
+        elif self.tokens[0] == 'endif': self.type = 'endif'
+        elif self.tokens[0] == 'for': self.type = 'for'
+        elif self.tokens[0] == 'endfor': self.type = 'endfor'
+        else: raise SyntaxError('Invalid expression')
 
 class Compiler(object):
     def __init__(self, file_or_string):
@@ -28,6 +38,7 @@ class Compiler(object):
         self.names = []
         self.varnames = []
         self.codes = []
+        self.codes_length = 0
         if isinstance(file_or_string, file):
             self.text = file_or_string.read()
         else:
@@ -53,17 +64,21 @@ class Compiler(object):
                 if len(args) < 1: raise ValueError('no enough argument')
                 self.codes.append(struct.pack('B' ,opcode.opmap[name]))
                 self.codes.append(struct.pack('H' ,args[0]))
+                self.codes_length += 3
             else:
                 self.codes.append(struct.pack('B' ,opcode.opmap[name]))
+                self.codes_length += 1
         return opfunc
 
     def tokenize(self):
-        tokens = re.split(r'({{[\ ]?[\w|_|.]+[\ ]?}})', self.text)
+        tokens = re.split(r'({[{%][\ ]?[\w_.\ ]+[\ ]?[}%]})', self.text)
+        if tokens[0] == '': tokens.pop(0)
+        if tokens[-1] == '': tokens.pop(-1)
         def format_token(t):
             if t.startswith('{{') and t.endswith('}}'):
                 return NameToken(t[2:-2].strip())
             elif t.startswith('{%') and t.endswith('%}'):
-                return SyntaxToken(t[2:-2])
+                return ExprToken(t[2:-2].strip())
             return RawToken(t)
         return map(format_token, tokens)
 
@@ -78,7 +93,7 @@ class Compiler(object):
                 self.LOAD_CONST(self._make_const(token))
                 self.CALL_FUNCTION(1)
                 self.POP_TOP()
-            if type(token) == NameToken:
+            elif type(token) == NameToken:
                 self.LOAD_FAST(pos_ret_list)
                 self.LOAD_ATTR(self._make_name('append'))
                 self.LOAD_GLOBAL(self._make_name(token.names[0]))
@@ -87,6 +102,13 @@ class Compiler(object):
                     self.BINARY_SUBSCR()
                 self.CALL_FUNCTION(1)
                 self.POP_TOP()
+            elif type(token) == ExprToken and token.type == 'if':
+                self.LOAD_GLOBAL(self._make_name(token.tokens[1]))
+                self.POP_JUMP_IF_FALSE(1)
+                self.mark = len(self.codes) - 1
+            elif type(token) == ExprToken and token.type == 'endif':
+                self.codes[self.mark] = struct.pack('H', self.codes_length)
+
 
 
         self.LOAD_CONST(self._make_const(''))
--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -9,8 +9,22 @@ class CompilerTestCase(unittest.TestCase):
         pass
 
     def test_tokenize(self):
+        c = compiler.Compiler('Hello, {{person.first_name}}, how are you?')
         wanted = ['Hello, ', compiler.NameToken('person.first_name'), ', how are you?']
-        self.assertListEqual(self.c.tokenize(), wanted)
+        self.assertListEqual(c.tokenize(), wanted)
+        c = compiler.Compiler('Hello, {% if a %}, how are you?{% endif %}')
+        wanted = ['Hello, ', compiler.ExprToken('if a'), ', how are you?', compiler.ExprToken('endif')]
+        self.assertListEqual(c.tokenize(), wanted)
+
+    def test_expr_token(self):
+        c = compiler.Compiler('Hi!, {%if True%}True{%endif%}')
+        tokens = c.tokenize()
+        self.assertEqual(tokens[1].type, 'if')
+        self.assertEqual(tokens[3].type, 'endif')
+
+    def test_codes_length(self):
+        c = compiler.Compiler('Hi!, {{name}}!')
+        self.assertEqual(len(c.codes), c.codes_length)
 
     def test_name_token(self):
         co = compiler.Compiler('Hello, {{name}}!').compile()
@@ -29,3 +43,10 @@ class CompilerTestCase(unittest.TestCase):
         person = {'first_name': 'Jim', 'last_name': 'Green'}
         ret = eval(self.c.compile(), {'person': person})
         self.assertEqual(wanted, ret)
+
+    def test_simple_if_expr(self):
+        c = compiler.Compiler('Hey,{%if fact%}it\'s true!{%endif%}oh!')
+        co = c.compile()
+        self.assertEqual(eval(co, {'fact': True}), 'Hey,it\'s true!oh!')
+        self.assertEqual(eval(co, {'fact': False}), 'Hey,oh!')
+
